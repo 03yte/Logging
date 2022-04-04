6f88c51d63e4399d3ba3742d7244a9e4433d9514
03yte <adrianbugnacki@gmail.com>
2022-04-04 11:14:32 -0400

    Create CrowdStrikeSensorCollector.sh

---
+++ CrowdStrikeSensorCollector.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Author: Adrian Bugnacki
+#V1.1 | Date: 03/30/2022
+#
+
+service="falcon-sensor"
+servicePresent=$(systemctl list-unit-files | grep ${service}.service)
+temp=${servicePresent[0]}
+hostname=$(cat /proc/sys/kernel/hostname)
+time=$(date '+%FT%T-%Z')
+
+if [ ${#temp} == 0 ]
+then
+        echo  "'{\"Time\":\"$time\",\"Hostname\":\"$hostname\", \"InitState\":\"NULL\",\"RunningState\":\"NULL\"}'"
+else
+systemdStat=($(systemctl status ${service}.service --quiet | awk '/Active:/{print $1 $3; next} /Loaded:/ {print substr($4, 1, length($4)-1);}'))
+        echo "'{\"Time\":\"$time\",\"Hostname\":\"$hostname\",\"InitState\":\"${systemdStat[0]}\",\"RunningState\":\"${systemdStat[1]}\"}'"
+fi
